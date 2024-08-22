09d795df6ecf482e8d5aa6f29a4711f296635370
arpansingh <arpansingh8583@gmail.com>
2024-08-22 16:47:38 +0530

    videoContainer

--- youtube/src/App.js
+++ youtube/src/App.js
@@ -1,29 +1,52 @@
-import { Provider } from 'react-redux';
-import './App.css';
-import Body from './components/Body';
-import Head from './components/Head';
-import store from './utils/store';
+import { Provider } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import Body from "./components/Body";
+import Head from "./components/Head";
+import MainContainer from "./components/MainContainer";
+import WatchPage from "./components/WatchPage";
+import store from "./utils/store";
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Body />,
+    children: [
+      {
+        path: "/",
+        element: <MainContainer />,
+      },
+      {
+        path: "watch",
+        element: <WatchPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="App">
-          <Head/>
-          <Body/>
+      <div>
+        <Head />
+        <RouterProvider router={appRouter} />
+
+        {/**
+         *
+         * Head
+         * Body
+         *  Sidebar
+         *    MenuItems
+         *  MainContainer
+         *    ButtonsList
+         *    VideoContainer
+         *      VideoCard
+         *
+         *
+         */}
       </div>
     </Provider>
-    
   );
 }
 
 export default App;
-{/**
-    head
-    body
-    sidebar
-        menuitems
-    main container
-        button list
-        videocontainer
-            videocard    
-    */}
