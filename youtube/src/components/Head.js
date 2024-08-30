82cbd77042c2e0ad8362cdfb0e17cdc1de8af00b
arpansingh <arpansingh8583@gmail.com>
2024-08-30 10:10:48 +0530

    search dynamic & jst slice made

--- youtube/src/components/Head.js
+++ youtube/src/components/Head.js
@@ -1,11 +1,20 @@
 import React, { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { toggleMenu } from '../utils/appSlice';
 import { YOUTUBE_SEARCH_API } from '../utils/constants';
 
 const Head = () => {
 
   const [searchQuery, setSearchQuery] = useState("");
+  const searchCache = useSelector((store) => store.search);
+  const [suggestions, setSuggestions] = useState([]);
+
+    /**
+   *  searchCache = {
+   *     "iphone": ["iphone 11", "iphone 14"]
+   *  }
+   *  searchQuery = iphone
+   */
 
   const dispatch = useDispatch();
   const toggleMenuHandler = () => {
@@ -13,14 +22,20 @@ const Head = () => {
   }
 
   useEffect(() => {
-    const timer = setTimeout(() => getSearchSuggestion(), 3000);
+    const timer = setTimeout(() => {
+      if (searchCache[searchQuery]) {
+        setSuggestions(searchCache[searchQuery]);
+      } else {
+        getSearchSuggestions();
+      }
+    }, 200);
 
-    return() => {
+    return () => {
       clearTimeout(timer);
     };
-  },[searchQuery]);
+  }, [searchQuery]);
 
-  const getSearchSuggestion = async () => {
+  const getSearchSuggestions = async () => {
     const options = {
       method: 'GET',
       headers: {
@@ -32,6 +47,65 @@ const Head = () => {
     const data = await fetch(`https://youtube-search-results.p.rapidapi.com/youtube-search/?q=${searchQuery}`, options);
     const json = await data.json();
     console.log(json.videos);
+    setSuggestions([
+    {
+        "id": "XaiDLr6utKI",
+        "title": "vbvbv 1",
+        "link": "https://youtu.be/XaiDLr6utKI",
+        "thumbnail": "https://i.ytimg.com/vi/XaiDLr6utKI/hqdefault.jpg",
+        "channel": {
+            "id": "UCWVqH8WfgnzHU9e-nqKBBEg",
+            "name": "sattan",
+            "link": "https://www.youtube.com/@sattan30",
+            "handle": "@sattan30",
+            "verified": false,
+            "thumbnail": "https://yt3.ggpht.com/ytc/AIdro_mo3g6cSXpMY2chk4O6xEnTVxdwX1U4OlP8WDmP6QjSU40=s0?imgmax=0"
+        },
+        "description": "My first 3D using Adobe After Efect.",
+        "views": 25,
+        "uploaded": "vor 10 Jahren",
+        "duration": 2,
+        "durationString": "0:02"
+    },
+    {
+        "id": "hW1yjMfFr68",
+        "title": "vbvbv",
+        "link": "https://youtu.be/hW1yjMfFr68",
+        "thumbnail": "https://i.ytimg.com/vi/hW1yjMfFr68/hqdefault.jpg",
+        "channel": {
+            "id": "UC_J6-BRT4kCuctzZ7IhpSIg",
+            "name": "Jarmane Jarmane",
+            "link": "https://www.youtube.com/@jarmanejarmane8236",
+            "handle": "@jarmanejarmane8236",
+            "verified": false,
+            "thumbnail": "https://yt3.ggpht.com/ytc/AIdro_nbdc5AzjivOkOVFs1t3A1g2QWi7OLsMZHV2ihZTqZmqltNd7G5En2x7TJbw2Ac3mb2Bw=s0?imgmax=0"
+        },
+        "description": "",
+        "views": 9,
+        "uploaded": "vor 2 Jahren",
+        "duration": 5,
+        "durationString": "0:05"
+    },
+    {
+        "id": "DwZ2mkHxSMQ",
+        "title": "vbvbv",
+        "link": "https://youtu.be/DwZ2mkHxSMQ",
+        "thumbnail": "https://i.ytimg.com/vi/DwZ2mkHxSMQ/hqdefault.jpg",
+        "channel": {
+            "id": "UC87AG4wZY6jMibfhUt05ndg",
+            "name": "MOlekata M103",
+            "link": "https://www.youtube.com/@molekatam1038",
+            "handle": "@molekatam1038",
+            "verified": false,
+            "thumbnail": "https://yt3.ggpht.com/ytc/AIdro_k1tvuTtgKI2_KoZXxZ5RLcZvsrAP8oHlgD1x4eKnA=s0?imgmax=0"
+        },
+        "description": "",
+        "views": 23,
+        "uploaded": "vor 8 Jahren",
+        "duration": 8,
+        "durationString": "0:08"
+    }
+    ]);
   }
 
   return (
@@ -49,21 +123,26 @@ const Head = () => {
         />
       </div>
       <div className='col-span-10 ml-36'>
-        <input
-        className='w-3/4 border border-gray-400 rounded-l-full p-2'
-        type='text'
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        <button className='border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100'>
-        🔍
-        </button>
+        <div>
+          <input
+            className='w-3/4 border border-gray-400 rounded-l-full p-2'
+            type='text'
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button className='border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100'>
+            🔍
+            </button>
+        </div>
       </div>
       <div className="fixed bg-white py-2 px-2 col-span-10 ml-[26rem] w-96 mt-10 shadow-lg rounded-lg border border-gray-100">
-          <ul>
-            <li className="py-2 px-3 shadow-sm hover:bg-gray-100">go</li>
-            <li className="py-2 px-3 shadow-sm hover:bg-gray-100">dhow</li>
-          </ul>
+      <ul>
+          {suggestions.map((s) => (
+            <li key={s.id} className="py-2 px-3 shadow-sm hover:bg-gray-100">
+              🔍 {s.title}
+            </li>
+          ))}
+      </ul>
       </div>
       <div className='col-span-1'>
         <img
--- youtube/src/utils/store.js
+++ youtube/src/utils/store.js
@@ -1,9 +1,11 @@
 import appSlice from "./appSlice";
 import { configureStore } from "@reduxjs/toolkit";
+import searchSlice from "./searchSlice";
 
 const store = configureStore({
     reducer: {
         app: appSlice,
+        search: searchSlice,
     }
 })
 
